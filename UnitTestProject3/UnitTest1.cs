48a1c4c83a666ac4e8520a3281273b2856bb7118
Adem Dinarević <ademdinarevic@gmail.com>
2017-05-21 11:38:54 +0200

    Selenium
    CS308

---
+++ UnitTestProject3/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+     
+        public static string siteURL = "https://google.com";
+
+        [TestMethod]
+        public void TestMethod2()
+
+        {
+            var driver = new ChromeDriver();
+            var otac;
+            driver.Navigate().GoToUrl(siteURL);
+
+            Task.Delay(2000).Wait();
+
+
+            var userName = driver.FindElement(By.Id("lst-ib"));
+
+            userName.SendKeys("ius sarajevo");
+
+            Task.Delay(2000).Wait();
+
+
+            var signIn = driver.FindElement(By.Name("btnK"));
+
+            signIn.Click();
+
+
+            Task.Delay(5000).Wait();
+
+
+            driver.Quit();
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "";
+
+            s = textBox1.Text + dateTimePicker1.Text;
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Title = "Save an Image File";
+
+            saveFileDialog1.ShowDialog();
+
+
+
+            if (saveFileDialog1.FileName != "")
+
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, s);
+        }
+    }
+}
